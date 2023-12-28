5ce676fd90421161e51daf10bd941e3fa0ffbb08
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-12-28 17:24:54 -0300

    aula 2 - promessas

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
+//fs (file system) é uma biblioteca nativa do js
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('Hello world!'));
+function trataErro(erro) {
+    console.log(erro);
+    //throw: lança uma mensagem de erro no terminal
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
 
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/');
-console.log('São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/');
+//async/await
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro);
+    }
+}
+
+//promises com then()
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     //primises: forma que o js trabalha com codigo assincrono
+//     //then é a ação que eu quero fazer depois que receber o retorno da função assíncrona, no caso o texto da função readFile
+//     //caso aconteça algum erro no then, ele é lançado dentro do catch
+//     fs.promises.readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch((erro) => trataErro(erro))
+// }
+
+//versão síncrona, sem promises
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     })
+// }
+
+pegaArquivo('./arquivos/texto.md');
