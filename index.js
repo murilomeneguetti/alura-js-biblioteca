ff968a45910f89eee9a9b08380ecef44073b890f
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-12-29 10:47:36 -0300

    aula 3 - regex

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}));
+
+    return resultados;
+}
+
 function trataErro(erro) {
     console.log(erro);
     //throw: lança uma mensagem de erro no terminal
@@ -13,7 +22,7 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(chalk.green(texto));
+        console.log(extraiLinks(texto));
     } catch (erro) {
         trataErro(erro);
     }
